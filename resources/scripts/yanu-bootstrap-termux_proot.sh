553a0172d880caec7ede063c83f22687adb7c597
raiyuu <raini4645@gmail.com>
2023-03-16 00:36:08 +0600

    Bash sensei sponsor (#5)
    - Improve error handling and minor corrections.
    - Thanks to Bash sensei.
    - Improve messages.
    
    ---------
    
    Co-authored-by: nozwock <57829219+nozwock@users.noreply.github.com>

--- resources/scripts/yanu-bootstrap-termux_proot.sh
+++ resources/scripts/yanu-bootstrap-termux_proot.sh
@@ -2,7 +2,7 @@
 
 # Handle errors
 err() {
-    echo -e "\e[;91mEncountered an error while trying to install yanu!\e[0m"
+    echo -e "\e[;91mEncountered an error while trying to install yanu! $(if [[ -n "$@" ]]; then echo -e "\nCause: $@"; fi)\e[0m"
     exit 1
 }
 
@@ -12,24 +12,24 @@ proot() {
     proot-distro login ubuntu -- "$@"
 }
 
-
 # Setting up deps
-termux-setup-storage || err
-pkg upgrade -y || err
-pkg in proot-distro || err
+termux-setup-storage || err "Failed to get permission to Internal storage."
+pkg update || err "Failed to sync package repos."
+pkg upgrade -y || err "Failed to update packages."
+pkg in proot-distro || err "Failed to install proot-distro."
 proot-distro install ubuntu || echo 'NOTE: `ubuntu` already installed.'
-proot apt update -y || err
-proot apt upgrade -y || err
-proot apt install git gcc binutils make || err
-proot rm -f /usr/bin/yanu || err
-proot curl -sLo /usr/bin/yanu https://github.com/nozwock/yanu/releases/latest/download/yanu-aarch64-termux_proot-linux-musl || err
-proot chmod +x /usr/bin/yanu || err
+proot apt update -y || err "Failed to sync package repos in proot."
+proot apt upgrade -y || err "Failed to update packages in proot."
+proot apt install git gcc binutils make || err "Failed to install required deps in proot."
+proot rm -f /usr/bin/yanu || err "Failed to clean up old yanu in proot."
+proot curl -sLo /usr/bin/yanu https://github.com/nozwock/yanu/releases/latest/download/yanu-aarch64-termux_proot-linux-musl || err "Failed to fetch yanu binary in proot."
+proot chmod +x /usr/bin/yanu || err "Failed to give executable permission."
 
-# Setting up proxy script
-rm -f "$PATH/yanu" || err
+# Setting up entry script
+rm -f "$PATH/yanu" || err "Failed to clean up old entry script."
 echo '
 #!/bin/bash
-proot-distro login --bind /storage/emulated/0 --termux-home ubuntu -- yanu "$@"' >> "$PATH/yanu" || err
-chmod +x "$PATH/yanu" || err
+proot-distro login --bind /storage/emulated/0 --termux-home ubuntu -- yanu "$@"' >>"$PATH/yanu" || err "Failed to write entry script."
+chmod +x "$PATH/yanu" || err "Failed to give executable permission."
 
-echo -e "\e[;92mInstall \`yanu\` successfully\nYou can run it by typing in\n\e[0m\e[;96myanu\e[0m"
+echo -e "\e[;92mInstalled \`yanu\` successfully\nYou can run it by typing in\n\e[0m\e[;96myanu\e[0m"
