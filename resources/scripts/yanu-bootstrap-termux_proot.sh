2d98f09d2686e2993cc32f33edd780c961096b5b
nozwock <57829219+nozwock@users.noreply.github.com>
2023-03-15 19:22:01 +0530

    Add yanu bootstrap script for termux build

---
+++ resources/scripts/yanu-bootstrap-termux_proot.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Handle errors
+err() {
+    echo -e "\e[;91mEncountered an error while trying to install yanu!\e[0m"
+    exit 1
+}
+
+# proot alias
+proot() {
+    echo -e "Running in PROOT:\n$*" # wtf is '$*'?
+    proot-distro login ubuntu -- "$@"
+}
+
+
+# Setting up deps
+termux-setup-storage || err
+pkg upgrade -y || err
+pkg in proot-distro || err
+proot apt update -y || err
+proot apt upgrade -y || err
+proot rm -f /usr/bin/yanu || err
+proot curl -sLo /usr/bin/yanu https://github.com/nozwock/yanu/releases/latest/download/yanu-aarch64-termux_proot-linux-musl || err
+proot chmod +x /usr/bin/yanu || err
+
+# Setting up proxy script
+rm -f "$PATH/yanu" || err
+echo '
+#!/bin/bash
+proot-distro login --bind /storage/emulated/0 --termux-home --isolated ubuntu -- yanu "$@"' >> "$PATH/yanu" || err
+chmod +x "$PATH/yanu" || err
+
+echo -e "\e[;92mInstall \`yanu\` successfully\nYou can run it by typing in\n\e[0m\e[;96myanu\e[0m"
