3715fece3e8318594b06345dca903f1af797f917
nozwock <57829219+nozwock@users.noreply.github.com>
2023-04-09 12:38:27 +0530

    Remove comment

--- libyanu-common/src/hac/backend.rs
+++ libyanu-common/src/hac/backend.rs
@@ -54,8 +54,6 @@ pub struct Backend {
     path: PathBuf,
 }
 
-// TODO: maybe a method to get a Command instance?
-
 impl Backend {
     pub fn new(kind: BackendKind) -> Result<Self> {
         let filename = kind.to_filename();
