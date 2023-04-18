nozwock/yanu

commit a0be156a37eb857066b92e7a71ce7b19c8ef13e1

Add 4nxci to `Backend`
Along with it added a build func with build first, embed second priority.

url: https://github.com/The-4n/4NXCI

crates/hac/src/backend.rs
@@ -4,9 +4,12 @@ use eyre::Result;
 use once_cell::sync::Lazy;
 #[cfg(all(target_arch = "x86_64", unix))]
 use std::ffi::OsStr;
-use std::path::{Path, PathBuf};
 #[cfg(unix)]
 use std::process::Command;
+use std::{
+    fmt,
+    path::{Path, PathBuf},
+};
 #[cfg(unix)]
 use tempfile::tempdir;
 
@@ -24,6 +27,7 @@ pub enum BackendKind {
     ))]
     Hactoolnet,
     Hac2l,
+    FourNXCI,
 }
 
 #[cfg(not(feature = "android-proot"))]
@@ -48,16 +52,9 @@ impl From<config::NcaExtractor> for BackendKind {
     }
 }
 
-impl BackendKind {
-    fn to_filename(&self) -> String {
-        #[cfg(unix)]
-        {
-            format!("{:?}", self).to_lowercase()
-        }
-        #[cfg(windows)]
-        {
-            format!("{:?}.exe", self).to_lowercase()
-        }
+impl fmt::Display for BackendKind {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
     }
 }
 
@@ -69,13 +66,14 @@ pub struct Backend {
 // TODO?: Some method in which backends are builded no matter what
 
 impl Backend {
+    /// Prefers embedded, builds only if binary not available.
     pub fn try_new(kind: BackendKind) -> Result<Self> {
-        let filename = kind.to_filename();
+        let filename = kind.to_string().to_lowercase();
         let cache = Cache::default();
-        let path = if let Ok(cached_path) = cache.path(&filename) {
+        let cached_path = if let Ok(cached_path) = cache.get(&filename) {
             cached_path
         } else {
-            #[cfg(all(target_arch = "x86_64", target_os = "windows"))]
+            #[cfg(windows)]
             {
                 match kind {
                     BackendKind::Hacpack => cache.store_bytes(defines::HACPACK, &filename)?,
@@ -87,21 +85,53 @@ impl Backend {
             #[cfg(unix)]
             {
                 let cached_path = match kind {
-                    BackendKind::Hacpack => cache.store_path(make_hacpack()?)?,
-                    BackendKind::Hactool => cache.store_path(make_hactool()?)?,
+                    #[cfg(all(target_arch = "x86_64", target_os = "linux"))]
+                    BackendKind::Hacpack => Backend::build(kind)?.path,
+                    #[cfg(all(target_arch = "x86_64", target_os = "linux"))]
+                    BackendKind::Hactool => Backend::build(kind)?.path,
                     #[cfg(all(target_arch = "x86_64", target_os = "linux"))]
                     BackendKind::Hactoolnet => cache.store_bytes(defines::HACTOOLNET, &filename)?,
                     #[cfg(all(target_arch = "x86_64", target_os = "linux"))]
-                    BackendKind::Hac2l => cache.store_path(make_hac2l(["linux_x64_release"])?)?,
+                    BackendKind::Hac2l => Backend::build(kind)?.path,
+                    #[cfg(feature = "android-proot")]
+                    BackendKind::Hacpack => cache.store_bytes(defines::HACPACK, &filename)?,
+                    #[cfg(feature = "android-proot")]
+                    BackendKind::Hactool => cache.store_bytes(defines::HACTOOL, &filename)?,
                     #[cfg(feature = "android-proot")]
                     BackendKind::Hac2l => cache.store_bytes(defines::HAC2L, &filename)?,
+                    BackendKind::FourNXCI => cache.store_bytes(defines::FOURNXCI, &filename)?,
                 };
                 set_executable_bit(&cached_path, true)?;
                 cached_path
             }
         };
 
-        Ok(Self { kind, path })
+        Ok(Self {
+            kind,
+            path: cached_path,
+        })
+    }
+    #[cfg(unix)]
+    /// Opposite of `try_new`.
+    pub fn build(kind: BackendKind) -> Result<Self> {
+        let cache = Cache::default();
+        let cached_path = match kind {
+            BackendKind::Hacpack => cache.store_path(build::hacpack()?)?,
+            BackendKind::Hactool => cache.store_path(build::hactool()?)?,
+            #[cfg(all(target_arch = "x86_64", target_os = "linux"))]
+            BackendKind::Hactoolnet => Backend::try_new(kind)?.path,
+            #[cfg(all(target_arch = "x86_64", target_os = "linux"))]
+            BackendKind::Hac2l => cache.store_path(build::hac2l(["linux_x64_release"])?)?,
+            #[cfg(feature = "android-proot")]
+            BackendKind::Hac2l => Backend::try_new(kind)?.path,
+            BackendKind::FourNXCI => cache.store_path(build::four_nxci()?)?,
+        };
+        set_executable_bit(&cached_path, true)?;
+
+        Ok(Self {
+            kind,
+            path: cached_path,
+        })
     }
     pub fn path(&self) -> &Path {
         &self.path
@@ -112,242 +142,271 @@ impl Backend {
 }
 
 #[cfg(unix)]
-static NPROC: Lazy<Result<u8>> = Lazy::new(|| {
-    Ok(String::from_utf8(Command::new("nproc").output()?.stdout)?
-        .trim()
-        .parse()?)
-});
-
-#[cfg(unix)]
-pub fn make_hacpack() -> Result<PathBuf> {
+pub mod build {
     use common::{defines::APP_CACHE_DIR, utils::move_file};
     use config::Config;
     use eyre::{bail, eyre};
     use fs_err as fs;
     use tracing::info;
 
-    let config = Config::load()?;
-    let name = format!("{:?}", BackendKind::Hacpack).to_lowercase();
-    info!("Building {}", name);
-    let src_dir = tempdir()?;
+    use super::*;
 
-    if !Command::new("git")
-        .args(["clone", "https://github.com/The-4n/hacPack"])
-        .arg(src_dir.path())
-        .status()?
-        .success()
-    {
-        bail!("Failed to clone {} repo", name);
-    }
+    static NPROC: Lazy<Result<u8>> = Lazy::new(|| {
+        Ok(String::from_utf8(Command::new("nproc").output()?.stdout)?
+            .trim()
+            .parse()?)
+    });
 
-    if !Command::new("git")
-        .args(["checkout", &config.hacpack_rev])
-        .current_dir(src_dir.path())
-        .status()?
-        .success()
-    {
-        bail!("Failed to checkout");
-    }
+    pub fn hacpack() -> Result<PathBuf> {
+        let config = Config::load()?;
+        let name = BackendKind::Hacpack.to_string();
+        info!("Building {}", name);
+        let src_dir = tempdir()?;
 
-    info!("Renaming config file");
-    fs::rename(
-        src_dir.path().join("config.mk.template"),
-        src_dir.path().join("config.mk"),
-    )?;
-
-    info!("Running make");
-    if !Command::new("make")
-        .args([
-            "-j",
-            &(NPROC.as_ref().map_err(|err| eyre!(err))? / 2).to_string(),
-        ])
-        .current_dir(&src_dir)
-        .status()?
-        .success()
-    {
-        bail!("Failed to build {}", name);
-    }
-
-    //* Moving bin from temp dir to cache dir
-    let filename = BackendKind::Hacpack.to_filename();
-    fs_err::create_dir_all(APP_CACHE_DIR.as_path())?;
-    let dest = APP_CACHE_DIR.join(&filename);
-    move_file(src_dir.path().join(&filename), &dest)?;
-
-    Ok(dest)
-}
-
-#[cfg(unix)]
-pub fn make_hactool() -> Result<PathBuf> {
-    use common::{defines::APP_CACHE_DIR, utils::move_file};
-    use config::Config;
-    use eyre::{bail, eyre};
-    use fs_err as fs;
-    use tracing::info;
-
-    let config = Config::load()?;
-    let name = format!("{:?}", BackendKind::Hactool).to_lowercase();
-    info!("Building {}", name);
-    let src_dir = tempdir()?;
-
-    if !Command::new("git")
-        .args(["clone", "https://github.com/SciresM/hactool"])
-        .arg(src_dir.path())
-        .status()?
-        .success()
-    {
-        bail!("Failed to clone {} repo", name);
-    }
-
-    if !Command::new("git")
-        .args(["checkout", &config.hactool_rev])
-        .current_dir(src_dir.path())
-        .status()?
-        .success()
-    {
-        bail!("Failed to checkout");
-    }
-
-    info!("Renaming config file");
-    fs::rename(
-        src_dir.path().join("config.mk.template"),
-        src_dir.path().join("config.mk"),
-    )?;
-
-    // removing line 372 as it causes build to fail on android
-    #[cfg(target_os = "android")]
-    {
-        use std::io::{BufRead, BufReader};
-
-        info!("Removing line 372 from `main.c`");
-        let reader = BufReader::new(fs::File::open(src_dir.path().join("main.c"))?);
-        //* can't use advance_by yet
-        let fixed_main = reader
-            .lines()
-            .enumerate()
-            .filter_map(|(i, ln)| {
-                if i != 371 {
-                    // i.e ln 372
-                    return Some(ln);
-                }
-                None
-            })
-            .collect::<Result<Vec<_>, _>>()?
-            .join("\n");
-        fs::write(src_dir.path().join("main.c"), fixed_main.as_bytes())?;
-    }
-
-    info!("Running make");
-    if !Command::new("make")
-        .args([
-            "-j",
-            &(NPROC.as_ref().map_err(|err| eyre!(err))? / 2).to_string(),
-        ])
-        .current_dir(&src_dir)
-        .status()?
-        .success()
-    {
-        bail!("Failed to build {}", name);
-    }
-
-    //* Moving bin from temp dir to cache dir
-    let filename = BackendKind::Hactool.to_filename();
-    fs_err::create_dir_all(APP_CACHE_DIR.as_path())?;
-    let dest = APP_CACHE_DIR.join(&filename);
-    move_file(src_dir.path().join(&filename), &dest)?;
-
-    Ok(dest)
-}
-
-#[cfg(all(target_arch = "x86_64", unix))]
-pub fn make_hac2l<I, S>(args: I) -> Result<PathBuf>
-where
-    I: IntoIterator<Item = S>,
-    S: AsRef<OsStr>,
-{
-    use common::{defines::APP_CACHE_DIR, utils::move_file};
-    use config::Config;
-    use eyre::{bail, eyre};
-    use tracing::{debug, info};
-
-    let config = Config::load()?;
-
-    let name = format!("{:?}", BackendKind::Hac2l).to_lowercase();
-    info!("Building {}", name);
-    let src_dir = tempdir()?;
-
-    if !Command::new("git")
-        .args(["clone", "https://github.com/Atmosphere-NX/Atmosphere.git"])
-        .arg(src_dir.path())
-        .status()?
-        .success()
-    {
-        bail!("Failed to clone Atmosphere repo");
-    }
-
-    if !Command::new("git")
-        .args(["checkout", &config.atmosphere_rev])
-        .current_dir(src_dir.path())
-        .status()?
-        .success()
-    {
-        bail!("Failed to checkout");
-    }
-
-    let hac2l_src_dir = src_dir.path().join("tools/hac2l");
-    if !Command::new("git")
-        .args(["clone", "https://github.com/Atmosphere-NX/hac2l.git"])
-        .arg(&hac2l_src_dir)
-        .status()?
-        .success()
-    {
-        bail!("Failed to clone {} repo", name);
-    }
-
-    if !Command::new("git")
-        .args(["checkout", &config.hac2l_rev])
-        .current_dir(&hac2l_src_dir)
-        .status()?
-        .success()
-    {
-        bail!("Failed to checkout");
-    }
-
-    info!("Running make");
-
-    if !Command::new("make")
-        .args([
-            "-j",
-            &(NPROC.as_ref().map_err(|err| eyre!(err))? / 2).to_string(),
-        ])
-        .args(args)
-        .current_dir(&hac2l_src_dir)
-        .status()?
-        .success()
-    {
-        bail!("Failed to build {}", name);
-    }
-
-    //* Moving bin from temp dir to cache dir
-    let filename = BackendKind::Hac2l.to_filename();
-    fs_err::create_dir_all(APP_CACHE_DIR.as_path())?;
-    let dest = APP_CACHE_DIR.join(&filename);
-    for entry in walkdir::WalkDir::new(hac2l_src_dir.join("out"))
-        .min_depth(1)
-        .contents_first(true)
-        .into_iter()
-        .filter_map(|e| e.ok())
-    {
-        debug!(
-            ?entry,
-            is_file = entry.file_type().is_file(),
-            parent_is_release = entry.path().parent().unwrap().ends_with("release")
-        );
-        if entry.file_type().is_file() && entry.path().parent().unwrap().ends_with("release") {
-            move_file(entry.path(), &dest)?;
-            return Ok(dest);
+        if !Command::new("git")
+            .args(["clone", "https://github.com/The-4n/hacPack"])
+            .arg(src_dir.path())
+            .status()?
+            .success()
+        {
+            bail!("Failed to clone {} repo", name);
         }
+
+        git_checkout(src_dir.path(), &config.hacpack_rev)?;
+
+        info!("Renaming config file");
+        fs::rename(
+            src_dir.path().join("config.mk.template"),
+            src_dir.path().join("config.mk"),
+        )?;
+
+        info!("Running make");
+        if !Command::new("make")
+            .args([
+                "-j",
+                &(NPROC.as_ref().map_err(|err| eyre!(err))? / 2).to_string(),
+            ])
+            .current_dir(&src_dir)
+            .status()?
+            .success()
+        {
+            bail!("Failed to build {}", name);
+        }
+
+        //* Moving bin from temp dir to cache dir
+        let filename = name.to_lowercase();
+        fs_err::create_dir_all(APP_CACHE_DIR.as_path())?;
+        let dest = APP_CACHE_DIR.join(&filename);
+        move_file(src_dir.path().join(&filename), &dest)?;
+
+        Ok(dest)
     }
 
-    bail!("Failed to build {}", name);
+    pub fn hactool() -> Result<PathBuf> {
+        let config = Config::load()?;
+        let name = BackendKind::Hactool.to_string();
+        info!("Building {}", name);
+        let src_dir = tempdir()?;
+
+        if !Command::new("git")
+            .args(["clone", "https://github.com/SciresM/hactool"])
+            .arg(src_dir.path())
+            .status()?
+            .success()
+        {
+            bail!("Failed to clone {} repo", name);
+        }
+
+        git_checkout(src_dir.path(), &config.hactool_rev)?;
+
+        info!("Renaming config file");
+        fs::rename(
+            src_dir.path().join("config.mk.template"),
+            src_dir.path().join("config.mk"),
+        )?;
+
+        // removing line 372 as it causes build to fail on android
+        #[cfg(target_os = "android")]
+        {
+            use std::io::{BufRead, BufReader};
+
+            info!("Removing line 372 from `main.c`");
+            let reader = BufReader::new(fs::File::open(src_dir.path().join("main.c"))?);
+            //* can't use advance_by yet
+            let fixed_main = reader
+                .lines()
+                .enumerate()
+                .filter_map(|(i, ln)| {
+                    if i != 371 {
+                        // i.e ln 372
+                        return Some(ln);
+                    }
+                    None
+                })
+                .collect::<Result<Vec<_>, _>>()?
+                .join("\n");
+            fs::write(src_dir.path().join("main.c"), fixed_main.as_bytes())?;
+        }
+
+        info!("Running make");
+        if !Command::new("make")
+            .args([
+                "-j",
+                &(NPROC.as_ref().map_err(|err| eyre!(err))? / 2).to_string(),
+            ])
+            .current_dir(&src_dir)
+            .status()?
+            .success()
+        {
+            bail!("Failed to build {}", name);
+        }
+
+        //* Moving bin from temp dir to cache dir
+        let filename = name.to_lowercase();
+        fs_err::create_dir_all(APP_CACHE_DIR.as_path())?;
+        let dest = APP_CACHE_DIR.join(&filename);
+        move_file(src_dir.path().join(&filename), &dest)?;
+
+        Ok(dest)
+    }
+
+    #[cfg(all(target_arch = "x86_64", target_os = "linux"))]
+    pub fn hac2l<I, S>(args: I) -> Result<PathBuf>
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        use tracing::debug;
+
+        let config = Config::load()?;
+
+        let name = BackendKind::Hac2l.to_string();
+        info!("Building {}", name);
+        let src_dir = tempdir()?;
+
+        if !Command::new("git")
+            .args(["clone", "https://github.com/Atmosphere-NX/Atmosphere.git"])
+            .arg(src_dir.path())
+            .status()?
+            .success()
+        {
+            bail!("Failed to clone Atmosphere repo");
+        }
+
+        git_checkout(src_dir.path(), &config.atmosphere_rev)?;
+
+        let hac2l_src_dir = src_dir.path().join("tools/hac2l");
+        if !Command::new("git")
+            .args(["clone", "https://github.com/Atmosphere-NX/hac2l.git"])
+            .arg(&hac2l_src_dir)
+            .status()?
+            .success()
+        {
+            bail!("Failed to clone {} repo", name);
+        }
+
+        if !Command::new("git")
+            .args(["checkout", &config.hac2l_rev])
+            .current_dir(&hac2l_src_dir)
+            .status()?
+            .success()
+        {
+            bail!("Failed to checkout");
+        }
+
+        info!("Running make");
+
+        if !Command::new("make")
+            .args([
+                "-j",
+                &(NPROC.as_ref().map_err(|err| eyre!(err))? / 2).to_string(),
+            ])
+            .args(args)
+            .current_dir(&hac2l_src_dir)
+            .status()?
+            .success()
+        {
+            bail!("Failed to build {}", name);
+        }
+
+        //* Moving bin from temp dir to cache dir
+        let filename = name.to_lowercase();
+        fs_err::create_dir_all(APP_CACHE_DIR.as_path())?;
+        let dest = APP_CACHE_DIR.join(&filename);
+        for entry in walkdir::WalkDir::new(hac2l_src_dir.join("out"))
+            .min_depth(1)
+            .contents_first(true)
+            .into_iter()
+            .filter_map(|e| e.ok())
+        {
+            debug!(
+                ?entry,
+                is_file = entry.file_type().is_file(),
+                parent_is_release = entry.path().parent().unwrap().ends_with("release")
+            );
+            if entry.file_type().is_file() && entry.path().parent().unwrap().ends_with("release") {
+                move_file(entry.path(), &dest)?;
+                return Ok(dest);
+            }
+        }
+
+        bail!("Failed to build {}", name);
+    }
+
+    pub fn four_nxci() -> Result<PathBuf> {
+        let config = Config::load()?;
+        let name = "4NXCI";
+        info!("Building {}", name);
+        let src_dir = tempdir()?;
+
+        if !Command::new("git")
+            .args(["clone", "https://github.com/The-4n/4NXCI.git"])
+            .arg(src_dir.path())
+            .status()?
+            .success()
+        {
+            bail!("Failed to clone {}", name)
+        }
+
+        git_checkout(src_dir.path(), &config.four_nxci_rev)?;
+
+        info!("Renaming config file");
+        fs::rename(
+            src_dir.path().join("config.mk.template"),
+            src_dir.path().join("config.mk"),
+        )?;
+
+        info!("Running make");
+        if !Command::new("make")
+            .args([
+                "-j",
+                &(NPROC.as_ref().map_err(|err| eyre!(err))? / 2).to_string(),
+            ])
+            .current_dir(&src_dir)
+            .status()?
+            .success()
+        {
+            bail!("Failed to build {}", name);
+        }
+
+        //* Moving bin from temp dir to cache dir
+        let filename = name.to_lowercase();
+        fs_err::create_dir_all(APP_CACHE_DIR.as_path())?;
+        let dest = APP_CACHE_DIR.join(&filename);
+        move_file(src_dir.path().join(&filename), &dest)?;
+
+        Ok(dest)
+    }
+
+    fn git_checkout<P: AsRef<Path>>(repo: P, rev: &str) -> Result<()> {
+        if Command::new("git")
+            .args(["checkout", rev])
+            .current_dir(repo)
+            .status()?
+            .success()
+        {
+            return Ok(());
+        }
+        bail!("Failed to checkout");
+    }
 }

crates/cache/src/lib.rs
@@ -46,7 +46,7 @@ impl Cache<'_> {
         Ok(file.path().into())
     }
     /// Looks for a file with `filename` in the cache dir and returns its path.
-    pub fn path(&self, filename: &str) -> Result<PathBuf> {
+    pub fn get(&self, filename: &str) -> Result<PathBuf> {
         for entry in walkdir::WalkDir::new(self.dir)
             .min_depth(1)
             .max_depth(1)

crates/common/src/defines.rs
@@ -12,13 +12,23 @@ pub const HACTOOL: &[u8] = include_bytes!("../../../assets/x86_64-windows/hactoo
 pub const HACTOOLNET: &[u8] = include_bytes!("../../../assets/x86_64-windows/hactoolnet.exe");
 #[cfg(all(target_arch = "x86_64", target_os = "windows"))]
 pub const HAC2L: &[u8] = include_bytes!("../../../assets/x86_64-windows/hac2l.exe");
+#[cfg(all(target_arch = "x86_64", target_os = "windows"))]
+pub const FOURNXCI: &[u8] = include_bytes!("../../../assets/x86_64-windows/4nxci");
 
 // Hactoolnet v0.18
 #[cfg(all(target_arch = "x86_64", target_os = "linux"))]
 pub const HACTOOLNET: &[u8] = include_bytes!("../../../assets/x86_64-linux/hactoolnet");
+#[cfg(all(target_arch = "x86_64", target_os = "linux"))]
+pub const FOURNXCI: &[u8] = include_bytes!("../../../assets/x86_64-linux/4nxci");
 
 #[cfg(feature = "android-proot")]
+pub const HACPACK: &[u8] = include_bytes!("../../../assets/aarch64-linux/hacpack");
+#[cfg(feature = "android-proot")]
+pub const HACTOOL: &[u8] = include_bytes!("../../../assets/aarch64-linux/hactool");
+#[cfg(feature = "android-proot")]
 pub const HAC2L: &[u8] = include_bytes!("../../../assets/aarch64-linux/hac2l");
+#[cfg(feature = "android-proot")]
+pub const FOURNXCI: &[u8] = include_bytes!("../../../assets/aarch64-linux/4nxci");
 
 pub static APP_CACHE_DIR: Lazy<PathBuf> =
     Lazy::new(|| dirs::cache_dir().unwrap_or_default().join(APP_DIR));

crates/config/src/lib.rs
@@ -42,6 +42,8 @@ pub struct Config {
     pub hac2l_rev: String,
     #[cfg(unix)]
     pub atmosphere_rev: String,
+    #[cfg(unix)]
+    pub four_nxci_rev: String,
 }
 
 impl Default for Config {
@@ -62,6 +64,8 @@ impl Default for Config {
             hac2l_rev: "7fc1b3a32c6a870c47d7459b23fd7c7b63014186".into(),
             #[cfg(unix)]
             atmosphere_rev: "1afb184c143f4319e5d6d4ea27260e61830c42a0".into(),
+            #[cfg(unix)]
+            four_nxci_rev: "33044e650ad58a72d231d9793383f5f279b73884".into(),
         }
     }
 }

crates/hac/src/lib.rs
@@ -1,3 +1,13 @@
+#[cfg(not(any(
+    all(
+        target_arch = "x86_64",
+        any(windows, unix),
+        not(feature = "android-proot")
+    ),
+    all(target_arch = "aarch64", feature = "android-proot")
+)))]
+compile_error!("This traget configuration is not supported");
+
 pub mod backend;
 pub mod utils;
 pub mod vfs;

crates/yanu-cli/src/main.rs
@@ -4,7 +4,7 @@ use std::{path::PathBuf, time::Instant};
 
 use clap::Parser;
 use common::{
-    defines::{APP_CONFIG_PATH, DEFAULT_PRODKEYS_PATH},
+    defines::{APP_CACHE_DIR, APP_CONFIG_PATH, DEFAULT_PRODKEYS_PATH},
     error::MultiReport,
     utils::ext_matches,
 };
@@ -388,16 +388,22 @@ fn run() -> Result<()> {
         }
         #[cfg(unix)]
         Some(opts::Commands::BuildBackend) => {
+            // TODO: add option for force build
             let mut res_pool = vec![
                 Backend::try_new(BackendKind::Hactool),
                 Backend::try_new(BackendKind::Hac2l),
                 Backend::try_new(BackendKind::Hacpack),
+                Backend::try_new(BackendKind::FourNXCI),
             ];
             #[cfg(all(target_arch = "x86_64", target_os = "linux"))]
             res_pool.push(Backend::try_new(BackendKind::Hactoolnet));
             let res_pool: Vec<_> = res_pool.into_iter().filter_map(|res| res.err()).collect();
             if res_pool.is_empty() {
-                eprintln!("{}", style("Successfully built backend!").green().bold());
+                eprintln!(
+                    "{} {}",
+                    style("Successfully built backend!").green().bold(),
+                    style(format!("({})", APP_CACHE_DIR.display())).bold().dim()
+                );
             } else {
                 let err = MultiReport::new(res_pool);
                 bail!(err.join("\n"));

commit fc3f1499568eb09f21180f8b80b189066bd46d46

fix(hac): broken on win due to no extension for bins
Regression cause in a0be156a37eb857066b92e7a71ce7b19c8ef13e1
Becuase of me deleting the `to_filename` func for some reason.

crates/hac/src/backend.rs
@@ -58,6 +58,20 @@ impl fmt::Display for BackendKind {
     }
 }
 
+impl BackendKind {
+    // This is important, don't remove it again!
+    fn filename(&self) -> String {
+        #[cfg(unix)]
+        {
+            format!("{}", self).to_lowercase()
+        }
+        #[cfg(windows)]
+        {
+            format!("{}.exe", self).to_lowercase()
+        }
+    }
+}
+
 pub struct Backend {
     kind: BackendKind,
     path: PathBuf,
@@ -68,7 +82,7 @@ pub struct Backend {
 impl Backend {
     /// Prefers embedded, builds only if binary not available.
     pub fn try_new(kind: BackendKind) -> Result<Self> {
-        let filename = kind.to_string().to_lowercase();
+        let filename = kind.filename();
         let cache = Cache::default();
         let cached_path = if let Ok(cached_path) = cache.get(&filename) {
             cached_path
@@ -159,8 +173,8 @@ pub mod build {
 
     pub fn hacpack() -> Result<PathBuf> {
         let config = Config::load()?;
-        let name = BackendKind::Hacpack.to_string();
-        info!("Building {}", name);
+        let kind = BackendKind::Hacpack;
+        info!("Building {}", kind);
         let src_dir = tempdir()?;
 
         if !Command::new("git")
@@ -169,7 +183,7 @@ pub mod build {
             .status()?
             .success()
         {
-            bail!("Failed to clone {} repo", name);
+            bail!("Failed to clone {} repo", kind);
         }
 
         git_checkout(src_dir.path(), &config.hacpack_rev)?;
@@ -190,11 +204,11 @@ pub mod build {
             .status()?
             .success()
         {
-            bail!("Failed to build {}", name);
+            bail!("Failed to build {}", kind);
         }
 
         //* Moving bin from temp dir to cache dir
-        let filename = name.to_lowercase();
+        let filename = kind.filename();
         fs_err::create_dir_all(APP_CACHE_DIR.as_path())?;
         let dest = APP_CACHE_DIR.join(&filename);
         move_file(src_dir.path().join(&filename), &dest)?;
@@ -204,8 +218,8 @@ pub mod build {
 
     pub fn hactool() -> Result<PathBuf> {
         let config = Config::load()?;
-        let name = BackendKind::Hactool.to_string();
-        info!("Building {}", name);
+        let kind = BackendKind::Hactool;
+        info!("Building {}", kind);
         let src_dir = tempdir()?;
 
         if !Command::new("git")
@@ -214,7 +228,7 @@ pub mod build {
             .status()?
             .success()
         {
-            bail!("Failed to clone {} repo", name);
+            bail!("Failed to clone {} repo", kind);
         }
 
         git_checkout(src_dir.path(), &config.hactool_rev)?;
@@ -258,11 +272,11 @@ pub mod build {
             .status()?
             .success()
         {
-            bail!("Failed to build {}", name);
+            bail!("Failed to build {}", kind);
         }
 
         //* Moving bin from temp dir to cache dir
-        let filename = name.to_lowercase();
+        let filename = kind.filename();
         fs_err::create_dir_all(APP_CACHE_DIR.as_path())?;
         let dest = APP_CACHE_DIR.join(&filename);
         move_file(src_dir.path().join(&filename), &dest)?;
@@ -280,8 +294,8 @@ pub mod build {
 
         let config = Config::load()?;
 
-        let name = BackendKind::Hac2l.to_string();
-        info!("Building {}", name);
+        let kind = BackendKind::Hac2l;
+        info!("Building {}", kind);
         let src_dir = tempdir()?;
 
         if !Command::new("git")
@@ -302,7 +316,7 @@ pub mod build {
             .status()?
             .success()
         {
-            bail!("Failed to clone {} repo", name);
+            bail!("Failed to clone {} repo", kind);
         }
 
         if !Command::new("git")
@@ -326,11 +340,11 @@ pub mod build {
             .status()?
             .success()
         {
-            bail!("Failed to build {}", name);
+            bail!("Failed to build {}", kind);
         }
 
         //* Moving bin from temp dir to cache dir
-        let filename = name.to_lowercase();
+        let filename = kind.filename();
         fs_err::create_dir_all(APP_CACHE_DIR.as_path())?;
         let dest = APP_CACHE_DIR.join(&filename);
         for entry in walkdir::WalkDir::new(hac2l_src_dir.join("out"))
@@ -350,13 +364,13 @@ pub mod build {
             }
         }
 
-        bail!("Failed to build {}", name);
+        bail!("Failed to build {}", kind);
     }
 
     pub fn four_nxci() -> Result<PathBuf> {
         let config = Config::load()?;
-        let name = "4NXCI";
-        info!("Building {}", name);
+        let kind = BackendKind::FourNXCI;
+        info!("Building {}", kind);
         let src_dir = tempdir()?;
 
         if !Command::new("git")
@@ -365,7 +379,7 @@ pub mod build {
             .status()?
             .success()
         {
-            bail!("Failed to clone {} repo", name)
+            bail!("Failed to clone {} repo", kind)
         }
 
         git_checkout(src_dir.path(), &config.four_nxci_rev)?;
@@ -386,11 +400,11 @@ pub mod build {
             .status()?
             .success()
         {
-            bail!("Failed to build {}", name);
+            bail!("Failed to build {}", kind);
         }
 
         //* Moving bin from temp dir to cache dir
-        let filename = name.to_lowercase();
+        let filename = kind.filename();
         fs_err::create_dir_all(APP_CACHE_DIR.as_path())?;
         let dest = APP_CACHE_DIR.join(&filename);
         move_file(src_dir.path().join(&filename), &dest)?;